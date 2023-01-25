443a1dfc8724e7c97f73ae6a753ac5c471834b5a
Abhishek Kumar <onlyabhi03@gmail.com>
2023-01-25 13:18:30 +0530

    Extern Concept

---
+++ Extern Concept/extern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"support.c"
+
+// extern void display();
+// int x = 10;
+
+// void main()
+// {
+//   display();
+// }
+
+extern int x;
+int main()
+{
+  printf("x = %d",x);
+}
---
+++ Extern Concept/support.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// extern int x;
+
+// void display()
+// {
+//   printf("Hello from support file");
+//   printf("\n%d",x);
+// }
+
+int x = 10;
+// int main()
+// {
+//   printf("support");
+// }
